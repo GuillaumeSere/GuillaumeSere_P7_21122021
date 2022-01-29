90f284bc27a08b95066d81a42f52f711c6a5a1d4
Guillaume Sere <guillaumesere@hotmail.fr>
2022-01-29 12:26:48 +0100

    fin de l'algo recherche foreach

--- asset/js/components/inputMainHandler.js
+++ asset/js/components/inputMainHandler.js
@@ -17,27 +17,25 @@ export const InputMainListener = () => {
     inputMain.addEventListener('input', () => {
 
         const mainInputValue = inputMain.value.toLowerCase();
-      
+        // recherche dans les descriptions et les ingredients
         if (mainInputValue.length > 2){
-           const resultDescription = result.filter(item => item.description.toLowerCase().includes(mainInputValue.toLowerCase()))
-           const resultName = result.filter(item => item.name.toLowerCase().includes(mainInputValue.toLowerCase()))
-         
-           let suggestion = "";
-        // recherche dans la description
-           resultDescription.forEach(resultItem => 
-                 suggestion += createCardsForDom(resultItem)
-             )
-         // recherche dans les names    
-             resultName.forEach(resultItem => 
-                suggestion += createCardsForDom(resultItem)
+           const resultInput = result.filter(item => 
+            item.name.toLowerCase().includes(mainInputValue) ||
+            item.description.toLowerCase().includes(mainInputValue) ||
+           item.ingredients.forEach(ingredient =>
+            ingredient.ingredient.toLowerCase().includes(mainInputValue) 
             )
-
+           )
+           let suggestion = "";
+           resultInput.forEach(resultItem => 
+            suggestion += createCardsForDom(resultItem)
+           )
              const cardMessage = document.querySelector('#cardMessage')
-             displayRecipes(resultDescription, resultName)
-             cardMessage.style.display = "block"
+             displayRecipes(resultInput)
+             cardMessage.style.display = "none"
         }else{
             displayRecipes(result)
         }
-       
+     
     })
 }
